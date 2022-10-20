bb4bb933a1dc68652394e08bca51acda2501ac3c
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-20 10:12:14 +0530

    findLargestNo

---
+++ largestNo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1
+{
+    public static class largestNo
+    {
+        public static void CheckLargestNumber()
+        {
+            Console.WriteLine("Find the largest of three Number");
+
+
+            Console.WriteLine("Input the 1st number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the 2nd number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the 3rd number:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine(" The 1st number is the greest among three");
+                }
+                else
+                {
+                    Console.WriteLine(" The 3rd number is the greest among three");
+                }
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine(" The 2nd number is the greest among three");
+            }
+            else
+            {
+                Console.WriteLine(" The 3rd number is the greest among three");
+            }
+
+        }
+    }
+}

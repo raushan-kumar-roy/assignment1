b313c4c0c975ccabaec4c0de92b077029a8aa414
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-20 10:11:13 +0530

    flipcoin

---
+++ flipcoin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1
+{
+    public static class flipcoin
+    {
+        public static void CheckPercentage()
+        {
+            int headcount = 0;
+            int tailcount = 0;
+            double headpercentage = 0;
+            double tailpercentage = 0;
+
+            Console.WriteLine("please enter the value for number of flip");
+            int numberofFlip = Convert.ToInt32(Console.ReadLine());
+
+            if (numberofFlip > 0)
+            {
+                for (int i = 0; i < numberofFlip; i++)
+                {
+                    Random random = new Random();
+                    int number = random.Next(2);
+                    Console.WriteLine(number);
+                    if (number == 0)
+                    {
+                        tailcount++;
+                    }
+                    else
+                    {
+                        headcount++;
+                    }
+
+                }
+                headpercentage = headcount * 100 / numberofFlip;
+                tailpercentage = tailcount * 100 / numberofFlip;
+
+                Console.WriteLine("headpercentage" + tailpercentage);
+                Console.WriteLine("tailpercentage" + headpercentage);
+            }
+        }
+    }
+}

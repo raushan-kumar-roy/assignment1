7105f693c3b82bfc5e2f59b16fd23c90e1e13bc2
raushan-kumar-roy <thorbtc0101@gmail.com>
2022-10-20 10:07:01 +0530

    outputs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace assignment_1
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            vowelCons.Display();
+            //evenOdd.checkEvenOdd();
+            //largestNo.CheckLargestNumber();
+            //quotientRem.CheckQuotientAndRemainder();
+            //swap.SwapingTheNumbers();
+            //factor.CheckPrimeFactor();
+            //flipcoin.CheckPercentage();
+            //Harmonic.CheckHarmonic();
+           // LeapYear.CheckYear();
+        }
+    }
+}

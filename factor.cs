3a565b57395bf8608887acacdd099c5384d659c1
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-20 10:10:44 +0530

    primeFactor

---
+++ factor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1
+{
+    public static class factor
+    {
+        public static void CheckPrimeFactor()
+        {
+            int a, b;
+            Console.WriteLine("Enter the number");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            for (b = 2; a > 1; b++)
+
+                if (a % b == 0)
+                {
+                    int x = 0;
+                    while (a % b == 0)
+                    {
+                        a /= b;
+                        x++;
+                    }
+                    Console.WriteLine($"{b} is prime factor {x} Times");
+                }
+
+        }
+    }
+}

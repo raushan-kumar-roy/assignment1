b0e0fbf9d251f39cd13b96750ac62777cbabaf66
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-20 10:12:57 +0530

    checkLeapYear

---
+++ LeapYear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1
+{
+    public static class LeapYear
+    {
+        public static void CheckYear()
+        {
+            Console.WriteLine("Enter the year");
+            int year = int.Parse(Console.ReadLine());
+            if ((year % 4 == 0) && (year % 100! == 0 || (year % 400 == 0)))
+            {
+                Console.WriteLine(year + "is leaf year");
+
+
+            }
+            else
+            {
+                Console.WriteLine(year + "is not leaf year");
+            }
+        }
+    }
+}

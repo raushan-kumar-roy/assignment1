ee84d8ffeadb9831fee8e15cb186bd43ea53cfb7
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-20 10:15:04 +0530

    checkVowelCons

---
+++ vowelCons.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1
+{
+    public static class vowelCons
+    {
+        public static void Display()
+        {
+            Console.WriteLine(" Enter any character");
+            char ch = Convert.ToChar(Console.ReadLine());
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine(ch + "is vowel");
+            }
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch == 'Z'))
+            {
+                Console.WriteLine(ch + " is consonant. ");
+            }
+            else
+            {
+                Console.WriteLine("Enter the correct character");
+            }
+        }
+    }
+}

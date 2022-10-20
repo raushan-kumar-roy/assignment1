c7264857117a5a9627cfb713ddb45501f36141f4
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-20 10:10:16 +0530

    checkEvenOdd

---
+++ evenOdd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1
+{
+    public static class evenOdd
+    {
+        public static void checkEvenOdd()
+        {
+            int number, remainder;
+            Console.WriteLine("Enter any number");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            remainder = number % 2;
+            if (remainder == 0)
+            {
+                Console.WriteLine("{0}is an  even Number", number);
+            }
+            else
+            {
+                Console.WriteLine("{0}is an odd Number", number);
+            }
+
+        }
+    }
+}

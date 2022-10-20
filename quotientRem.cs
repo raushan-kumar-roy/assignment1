9524dba3f3346c7ca55288343e44162524e0f8a7
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-20 10:13:27 +0530

    getQuotientRem

---
+++ quotientRem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1
+{
+    public static class quotientRem
+    {
+        public static void CheckQuotientAndRemainder()
+        {
+            int divident = 50, divisor = 8;
+
+            int quotient = divident / divisor;
+            int remainder = divident % divisor;
+            Console.WriteLine("Divident : {0} Divisor : {1}", divident, divisor);
+            Console.WriteLine("Quotient : + quotient");
+            Console.WriteLine("Remainder : " + remainder);
+        }
+    
+}
+}

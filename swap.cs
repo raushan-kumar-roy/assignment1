ceb5eed4e59fd5834e4c447f99c5a467a1b1911f
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-10-20 10:14:30 +0530

    swapNos

---
+++ swap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1
+{
+    public static class swap
+    {
+        public static void SwapingTheNumbers()
+        {
+            int a = 5, b = 10;
+            Console.WriteLine("Before swap a = " + a + " b = " + b);
+
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+            Console.WriteLine("After swap a = " + a + " b = " + b);
+        }
+    
+}
+}
